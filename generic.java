ef249b3888e8a840fff2d5e16e8dbf3735367dca
Prithwiraj Mazumdar <143126729+Prithwiraj731@users.noreply.github.com>
2025-11-18 00:28:52 +0530

    Add Main class with ArrayList and HashMap examples

---
+++ generic.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Beluga");
+        names.add("Asha");
+        names.add("Ravi");
+
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "Java");
+        map.put(2, "Python");
+        map.put(3, "C++");
+        System.out.println("Map: " + map);
+    }
+}
